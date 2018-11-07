f64e497c655811d3885eb645662521c8f0439f49
stadmin <stadmin@nyc.yearup.org>
2018-11-07 15:11:22 -0500

    last commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function myFunction1() {
+  document.getElementById("demo1").innerHTML = "Paid";
+}
+
+function myFunction2() {
+  document.getElementById("demo2").innerHTML = "Paid";
+}
+
+function myFunction3() {
+  document.getElementById("demo3").innerHTML = "Paid";
+}
+
+function myFunction4() {
+  document.getElementById("demo4").innerHTML = "Paid";
+}
