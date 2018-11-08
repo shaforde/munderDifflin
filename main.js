aacc8bc22e010d41b4a82a3fa40b54b8557be6a6
stadmin <stadmin@nyc.yearup.org>
2018-11-08 16:31:39 -0500

    add semantic tags

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 function myFunction1() {
-  document.getElementById("demo1").innerHTML = "Paid";
+  document.getElementById("demo1").innerHTML = "Added to Cart!";
 }
 
 function myFunction2() {
-  document.getElementById("demo2").innerHTML = "Paid";
+  document.getElementById("demo2").innerHTML = "Added to Cart!";
 }
 
 function myFunction3() {
-  document.getElementById("demo3").innerHTML = "Paid";
+  document.getElementById("demo3").innerHTML = "Added to Cart!";
 }
 
 function myFunction4() {
-  document.getElementById("demo4").innerHTML = "Paid";
+  document.getElementById("demo4").innerHTML = "Added to Cart!";
+}
+
+function Submission(){
+  document.getElementById("form").innerHTML = "Submitted! One of our agents will contact you shortly.";
 }
